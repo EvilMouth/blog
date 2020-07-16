dd293df158e918038d3a8e06dfd2ae379dae26d3
zyhang <izyhang@gmail.com>
2020-07-16 14:39:47 +0800

    fix sh

--- pushSubmoduleHash.sh
+++ pushSubmoduleHash.sh
@@ -1,9 +1,10 @@
 status=`git status`
-if [[ $status =~ "source/_posts" || $status =~ "themes/mynext" ]]
+if [[ $status =~ "source/_posts" || $status =~ "themes/mynext" || $status =~ "themes/mycactus" ]]
 then
   echo 'there you go'
   git add source/_posts
   git add themes/mynext
+  git add themes/mycactus
   echo 'commiting...'
   git commit -m 'auto update submodule hash'
   echo 'Push rightnow? Type 'n' to stop, type any to continue'
