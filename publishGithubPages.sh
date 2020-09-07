114ed1c76a54bfd127d939a0107ce62cb12cde77
EVIL MOUTH <izyhang@gmail.com>
2020-09-07 15:44:34 +0800

    EvilMouth

--- publishGithubPages.sh
+++ publishGithubPages.sh
@@ -2,7 +2,7 @@ url=$(git remote get-url origin)
 cd public
 git init
 git config user.name "CI"
-git config user.email "ci@zyhang.com"
+git config user.email "ci@evilmouth.net"
 git remote add secure-origin "$url"
 git checkout -b gh-pages
 git add .
